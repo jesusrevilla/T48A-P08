11946073ec940a756f7bec7be40d7320976a41a6
Daniel Alejandro Venegas <110883720+DanielxVenegas@users.noreply.github.com>
2025-10-09 19:47:37 -0600

    Update model.py

--- naive-bayes-classifier/src/model.py
+++ naive-bayes-classifier/src/model.py
@@ -13,7 +13,7 @@ X = df["text"]
 y = df["label"]
 
 # Dividir en entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
 # Crear pipeline
 model = make_pipeline(CountVectorizer(), MultinomialNB())
