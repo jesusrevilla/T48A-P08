4ed2e641893cfcc7fe72698a3ad005103145a1cb
Gozuro <160263006+Gozuro@users.noreply.github.com>
2025-10-10 18:44:30 -0600

    Update_171877 model.py

--- naive-bayes-classifier/src/model.py
+++ naive-bayes-classifier/src/model.py
@@ -1,3 +1,4 @@
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
